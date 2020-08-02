9afc25bba763d353bc73a991260e806b8969ed35
grpargeter <pargetergregr@johndeere.com>
2020-08-02 16:22:11 -0500

    updated columns for users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const UserModel = require("../models").User;
 
 // GET USERS PROFILE
@@ -9,6 +8,27 @@ router.get("/profile/:id", (req, res) => {
     res.render("users/profile.ejs", {
       user: userProfile,
     });
+  });
+});
+
+//Edit Profile
+router.get("/:id/edit", (req, res) => {
+  Users.findByPk(req.params.id).then((users) => {
+    res.render("edit.ejs", {
+      user: userProfile,
+    });
+  });
+});
+router.put("/profile/:id", (req, res) => {
+  User.update(req.body, { where: { id: req.params.id } }).then(() => {
+    res.redirect(`/users/profile/${req.params.id}`);
+  });
+});
+
+//Delete route
+router.delete("/:id", (req, res) => {
+  User.destroy({ where: { id: req.params.id } }).then(() => {
+    res.redirect("/users");
   });
 });
 
